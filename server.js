c2ab7c3c037784108055f80d0d3ff4359915abcb
vvilliam312 <william.lasiila@gmail.com>
2019-08-23 10:23:20 +0300

    added new files

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express")
+var app = express()
+var db = require("./database.js")
+
+// var bodyParser = require("body-parser");
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+var HTTP_PORT = 8000
+
+// Start server
+app.listen(HTTP_PORT, () => {
+    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
+});
+
+app.use(express.static('public'))
+
+// Root path
+app.get("/", (req, res, next) => {
+    res.json({"message":"Ok"})
+});
