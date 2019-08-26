576b02804d165f79e956a3a4820e6d314554e50f
vvilliam312 <31401950+vvilliam312@users.noreply.github.com>
2019-08-26 08:17:30 +0300

    Add files via upload

--- database.js
+++ database.js
@@ -1,16 +1,18 @@
 var sqlite3 = require('sqlite3').verbose()
 
 const DBSOURCE = "nationality.db" 
+
+
 let db = new sqlite3.Database(DBSOURCE, (err) => {
     if (err) {
       // Cannot open database
-      console.log("FEL?!")
       console.error(err.message)
       throw err
     }else{
         console.log('Connected to the SQlite database.')
-        
     }
 })
+
+
 module.exports = db
 
--- server.js
+++ server.js
@@ -13,9 +13,46 @@ app.listen(HTTP_PORT, () => {
     console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
 });
 
-app.use(express.static('public'))
+// app.use(express.static('public'))
+
 
 // Root path
 app.get("/", (req, res, next) => {
-    res.json({"message":"Ok"})
+    res.json({
+        "message":"success",
+       
+    })  
+});
+
+app.get("/getall", (req, res, next) => {
+    var sql = `SELECT users.name,users.lastname,country.country_name,nationality.origin FROM users INNER JOIN country
+    ON users.country_id = country.id
+    INNER JOIN nationality
+    ON users.nationality_id = nationality.id`
+    var params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        } 
+        res.json({
+            "message":"success",
+            "data":rows
+        })
+      });
+});
+
+app.get("/all", (req, res, next) => {
+    var sql = `SELECT users.name,users.lastname FROM users`
+    var params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        } 
+        res.json({
+            "message":"success",
+            "data":rows
+        })
+      });
 });
